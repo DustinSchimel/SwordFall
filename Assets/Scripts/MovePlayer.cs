4aabff9577913c1bc34663242275783f58f6c5f6
Dustin Schimel <dustin.schimel@gmail.com>
2021-09-17 23:55:13 -0600

    Fixed collision issues

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -4,23 +4,56 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-    public float multiplier = 20;
+    public float speed = 20f;
+    public Rigidbody rb;
+    public Vector3 movement;
+
+    private bool leftMovement = true;
+    private bool rightMovement = true;
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-        {
-            this.transform.Translate(Vector3.right * Time.deltaTime * multiplier);
-        }
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-        {
-            this.transform.Translate(Vector3.left * Time.deltaTime * multiplier);
-        }
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0);
+    }
 
-        this.GetComponent<Rigidbody>().drag -= 0.00001f;
+    private void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
 
-        // Debug logs for sanity
-        // Debug.Log(this.transform.position.y);
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Left Wall"))
+        {
+            leftMovement = false;
+        }
+        else if (other.tag.Equals("Right Wall"))
+        {
+            rightMovement = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        leftMovement = true;
+        rightMovement = true;
+    }
+
+    void moveCharacter(Vector3 direction)
+    {
+        if ((Input.GetAxis("Horizontal") > 0) && rightMovement)    // Right
+        {
+            rb.MovePosition(transform.position + (direction * speed * Time.deltaTime));
+        }
+        if ((Input.GetAxis("Horizontal") < 0) && leftMovement)    // Left
+        {
+            rb.MovePosition(transform.position + (direction * speed * Time.deltaTime));
+        }
     }
 }
