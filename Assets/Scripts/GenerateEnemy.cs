9939fb94e68d9d637fed5de4914a59878f2ecb62
RealCrazyMage <50674427+RealCrazyMage@users.noreply.github.com>
2021-09-18 22:48:03 -0600

    Created a Pumpkin Script
    - As seen, it's not ready I just want to make sure the commit is still ok
    - Working on making particle effect for destroyed pumpkin

--- Assets/Scripts/GenerateEnemy.cs
+++ Assets/Scripts/GenerateEnemy.cs
@@ -21,12 +21,11 @@ public class GenerateEnemy : MonoBehaviour
         // Generate random integer to determine spawn rate of enemies and a random position for them to spawn
         System.Random rnd_gen = new System.Random();
         int rnd_int = rnd_gen.Next(0, gen_spd);
-        int rnd_pos = rnd_gen.Next(1237, 1370);
+        int rnd_pos = rnd_gen.Next(-35, 30);
         if (rnd_int == 7)
         {
             gen_spd--;
-
-            GameObject temp_enemy = GameObject.Instantiate(enemy, new Vector3(rnd_pos, player.transform.position.y - 100, -1177), Quaternion.identity);
+            GameObject temp_enemy = GameObject.Instantiate(enemy, new Vector3(rnd_pos, player.transform.position.y - 100, 0), Quaternion.identity);
             temp_enemy.GetComponent<Enemy>().player = this.player;
             rnd_int = 0;
         }
